1c907a47e924fee015747ea98abc39412c2bbcf2
Anais Mace <anaismace@yahoo.fr>
2020-06-07 01:08:21 +0200

    fix column and header

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -6,23 +6,28 @@ import { locales } from "../constants/locales";
 import "react-virtualized/styles.css";
 
 import { AutoSizer, Column, Table } from "react-virtualized";
+import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   body: {
     width: "100%",
     height: "82vh",
-  },
-  row: {
-    display: "flex",
   },
   cell: {
     display: "flex",
     flex: 1,
   },
-  headerLabel: {
-    fontWeight: 600,
+  header: {
+    padding: theme.spacing(2),
+    width: "100%",
   },
-});
+  column: {
+    textTransform: "none",
+  },
+  row: {
+    borderBottom: "solid black 1px",
+  },
+}));
 
 export default function ListContainer() {
   const classes = useStyles();
@@ -46,45 +51,52 @@ export default function ListContainer() {
   const getData = ({ index }) => baptisteriesInfo[index];
 
   return (
-    <div className={classes.body}>
+    <Paper className={classes.body}>
       <AutoSizer>
         {({ height, width }) => (
           <Table
             height={height}
             width={width}
-            headerHeight={45}
+            headerHeight={80}
             rowCount={nbBaptisteries}
-            rowHeight={45}
+            rowHeight={60}
             rowGetter={getData}
+            headerClassName={classes.header}
+            rowClassName={classes.row}
           >
             <Column
               className={classes.cell}
               label={locales.labelBaptisteryName[language]}
               dataKey={"name"}
+              headerClassName={classes.column}
             />
             <Column
               className={classes.cell}
               label={locales.labelBaptisteryRegion[language]}
               dataKey={"region"}
+              headerClassName={classes.column}
             />
             <Column
               className={classes.cell}
               label={locales.labelBaptisteryDioceseCivil[language]}
               dataKey={"civilDiocese"}
+              headerClassName={classes.column}
             />
             <Column
               className={classes.cell}
               label={locales.labelBaptisteryStartingYear[language]}
               dataKey={"startingYear"}
+              headerClassName={classes.column}
             />
             <Column
               className={classes.cell}
               label={locales.labelBaptisteryFinalYear[language]}
               dataKey={"finalYear"}
+              headerClassName={classes.column}
             />
           </Table>
         )}
       </AutoSizer>
-    </div>
+    </Paper>
   );
 }
